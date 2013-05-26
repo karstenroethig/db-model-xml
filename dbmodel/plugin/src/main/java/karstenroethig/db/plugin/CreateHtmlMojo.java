96d6da66d3ba4adfa2a58568a0f71371c2c205fd
Karsten Roethig <karsten.roethig@googlemail.com>
2013-05-26 13:24:39 +0200

    added creation of overview page for html output

--- dbmodel/plugin/src/main/java/karstenroethig/db/plugin/CreateHtmlMojo.java
+++ dbmodel/plugin/src/main/java/karstenroethig/db/plugin/CreateHtmlMojo.java
@@ -28,6 +28,11 @@ public class CreateHtmlMojo extends AbstractCreateWithVelocityMojo {
         required = true
     )
     private File outputDirectory;
+	
+	@Parameter(
+		defaultValue = "${project.artifactId}"
+	)
+	private String artifactId;
 
     private ResourceLocator resourceLocator = new HtmlResourceLocator();
 
@@ -37,19 +42,55 @@ public class CreateHtmlMojo extends AbstractCreateWithVelocityMojo {
     	
     	Database database = getDatebase();
 
-        try {
+        getLog().info( "creating 'single'" );
+        createHtml( database, new File( outputDirectory, "single"), false );
+        
+        getLog().info( "creating 'overview'" );
+        createHtml( database, new File( outputDirectory, "overview"), true );
+    }
+    
+    private void createHtml( Database database, File outputDirectory, boolean withOverview ) throws MojoExecutionException {
+    	
+    	String artifactVersion = artifactId + "-" + database.getVersion();
+    	
+    	try {
 
             copyFile( "print.png", outputDirectory, "resources", null );
             copyFile( "database.png", outputDirectory, "resources", null );
             copyFile( "consulting.png", outputDirectory, "resources", null );
             copyFile( "style.css", outputDirectory, "resources", null );
+            
+            if( withOverview ) {
+            	
+            	String targetSubDirectory = artifactVersion + "/resources";
+            	
+            	copyFile( "print.png", outputDirectory, targetSubDirectory, null );
+                copyFile( "database.png", outputDirectory, targetSubDirectory, null );
+                copyFile( "consulting.png", outputDirectory, targetSubDirectory, null );
+                copyFile( "style.css", outputDirectory, targetSubDirectory, null );
+            }
 
         } catch( IOException ex ) {
             throw new MojoExecutionException( "Error copying files", ex );
         }
+    	
+    	try {
+    		
+    		/*
+    		 * Overview
+    		 */
+    		if( withOverview ) {
 
-        try {
+        		Map<String, Object> overviewParams = new HashMap<String, Object>();
 
+        		overviewParams.put( "artifactVersion", artifactVersion );
+
+                evaluateFileWithVelocity( "overview.html", outputDirectory, null, "index.html", overviewParams );
+    		}
+
+    		/*
+    		 * Database
+    		 */
             List<String> entityPropertyKeys = new ArrayList<String>();
 
             for( Entity entity : database.getEntities() ) {
@@ -69,9 +110,17 @@ public class CreateHtmlMojo extends AbstractCreateWithVelocityMojo {
             Map<String, Object> databaseParams = new HashMap<String, Object>();
 
             databaseParams.put( "entityPropertyKeys", entityPropertyKeys );
+            databaseParams.put( "withOverview", withOverview );
+            
+            String targetSubDirectory = withOverview ? artifactVersion : null;
 
-            evaluateFileWithVelocity( "database.html", outputDirectory, null, "index.html", databaseParams );
+            evaluateFileWithVelocity( "database.html", outputDirectory, targetSubDirectory, "index.html", databaseParams );
 
+            /*
+             * Entities
+             */
+            targetSubDirectory = withOverview ? artifactVersion + "/entities" : "entities";
+            
             for( Entity entity : database.getEntities() ) {
 
                 List<String> attributePropertyKeys = new ArrayList<String>();
@@ -97,15 +146,15 @@ public class CreateHtmlMojo extends AbstractCreateWithVelocityMojo {
                 entityParams.put( "entity", entity );
                 entityParams.put( "attributePropertyKeys", attributePropertyKeys );
                 entityParams.put( "datatypeFormatter", datatypeFormatter );
+                entityParams.put( "withOverview", withOverview );
 
-                evaluateFileWithVelocity( "entity.html", outputDirectory, "entities", entity.getName() + ".html",
+                evaluateFileWithVelocity( "entity.html", outputDirectory, targetSubDirectory, entity.getName() + ".html",
                     entityParams );
             }
-
-        } catch( Exception ex ) {
+    		
+    	} catch( Exception ex ) {
             throw new MojoExecutionException( "Error creating files", ex );
         }
-
     }
     
     @Override
